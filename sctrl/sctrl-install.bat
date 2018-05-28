2e1dd7eb2acb2eca362b885f50ae44af74c20d56
Centny <centny@gmail.com>
2018-05-28 10:54:38 +0800

    fix the show log is not enabled now.

--- sctrl/sctrl-install.bat
+++ sctrl/sctrl-install.bat
@@ -3,7 +3,7 @@ cd /d %~dp0
 set /p name=name:
 set /p token=auth:
 mkdir logs
-nssm install "Sctrl Slaver" %CD%\sctrl.exe -sc -master rs.dyang.org:9121 -auth %token% -name %name% -showlog=1 -cert=certs/server.pem -key=certs/server.key 
+nssm install "Sctrl Slaver" %CD%\sctrl.exe -sc -master rs.dyang.org:9110 -auth %token% -name %name% -cert=certs/server.pem -key=certs/server.key 
 nssm set "Sctrl Slaver" AppStdout %CD%\logs\out.log
 nssm set "Sctrl Slaver" AppStderr %CD%\logs\err.log
 nssm start "Sctrl Slaver"
